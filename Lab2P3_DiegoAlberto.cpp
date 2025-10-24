e19d90ec8124318709f44648a9bcb24058f4d0f2
Diego122004 <daof.04@mail.com>
2025-10-24 14:46:07 -0600

    Implementacion de el menu y el primer ejercicio

--- Lab2P3_DiegoAlberto.cpp
+++ Lab2P3_DiegoAlberto.cpp
@@ -2,18 +2,78 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int opcion;
+    do {
+        cout << "MENU" << endl;
+        cout << "0. Salir" << endl;
+        cout << "1. Números primos" << endl;
+        cout << "2. Números amigables" << endl;
+        cout << "3. Serie de Fibonacci" << endl;
+        cin >> opcion;
+        switch (opcion != 0)
+        {
+        case 1: {
+            int n;
+            int contDePrimos = 0;
+            int contDeDiv = 0;
+
+            do {
+                cout << "Ingrese un valor de n mayor a 1\n";
+                cin >> n;
+
+                for (int i = 2; i < n; i++) {
+
+                    for (int j = 1; j <= i; j++) {
+
+                        if (i % j == 0) {
+                            contDeDiv++;
+                        }
+                    }
+                    if (contDeDiv == 2) {
+                        contDePrimos++;
+                    }
+                    contDeDiv = 0;
+                }
+
+                //cout << "El numero de primos en" <<" " << n << "Es " << " " << contDePrimos;
+
+
+
+                cout << "El numero de primos entre" << " " << "0" << " " << " y" << " " << n << " " << "Es " << contDePrimos << "\n";
+
+                cout << "Ingrese un valor de n mayor a 1\n";
+                cin >> n;
+
+            } while (n <= 1 && n != 0 && n >= 1);
+
+            if (n == 0) {
+                cout << "El programa a terminado";
+            }
+            break;
+        };
+
+        case 2: {
+
+
+
+        };
+              break;
+        case 3: {
+           
+        };
+              break;
+        case 0: {
+
+            };
+              break;
+        default:
+           
+            break;
+        };
+    } while (opcion != 0);
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
